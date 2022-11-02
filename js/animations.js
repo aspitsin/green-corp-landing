d221b4a7641a181655742a873b060816fe1b5a8f
aspitsin <alexs.spitsin@gmail.com>
2022-11-02 15:13:22 +0300

    add: animation

---
+++ js/animations.js
@@ -0,0 +1,86 @@
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+let animationInited = false;
+
+function increaseNumberAnimationStep (i, element, endNumber) {
+  if (i <= endNumber && !animationInited) {
+    if (i === endNumber) {
+      animationInited = true;
+      element.innerText = i + '+';
+    } else {
+      element.innerText = i;
+    }
+
+      i+=100;
+
+      setTimeout(function() {
+        increaseNumberAnimationStep(i, element, endNumber);
+      }, INCREASE_NUMBER_ANIMATION_SPEED);
+    }
+  }
+
+function initIncreaseNumberAnimation() {
+    const element = document.querySelector('.features__clients-count');
+    
+    increaseNumberAnimationStep(0, element, 5000);
+  }
+
+// ...
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+ 
+    formContainer.appendChild(input);
+    document.querySelector('#form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+  }
+
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) {
+  document.querySelector('#form').removeChild(otherInput);
+
+  }
+});
+
+function updateScroll() {
+  if (window.scrollY > 0 && !animationInited ) {
+
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+
+  if(windowBottomPosition >= countElementPosition){
+    initIncreaseNumberAnimation();
+  }
+}
+
+window.addEventListener('scroll', updateScroll);
+
+// ...
+
+function onLinkClick(event) {
+  event.preventDefault();
+ 
+  document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  }); 
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor)
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', onLinkClick);
+}
